4b8a630e253bc13e44273225ebd6bcbd5c1a1ab8
shashank shukla <dsshuklashashank@gmail.com>
2025-11-10 19:04:38 +0530

    Update

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={styles.container}
-    >
+      style={styles.container}>
       <Text style = {styles.content}>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
