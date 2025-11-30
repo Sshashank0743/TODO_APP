9936b22fcf1e7bd57531239a30a9cb51ea647f81
shashank shukla <dsshuklashashank@gmail.com>
2025-11-30 22:24:51 +0530

    Update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
@@ -7,7 +6,6 @@ export default function Index() {
       style={styles.container}>
       <Text style = {styles.content}>Edit app/index.tsx to edit this screen.</Text>
 
-      <Link>Visit about screen</Link>
     </View>
   );
 }
---
+++ hooks/useTeme.tsx
@@ -0,0 +1,18 @@
+
+export interface ColorScheme {
+    bg: String,
+    surface: String,
+    text: String,
+    textMuted: String,
+    border: String,
+    primary: String,
+    success: String,
+    warning: String,
+    danger: String,
+    
+}
+
+const useTeme = () => {
+}
+
+export default useTeme
