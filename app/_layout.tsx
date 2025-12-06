84dad887ea8c2e1e02563aaa9a7acfbc23552789
shashank shukla <s.shukla9919@gmail.com>
2025-12-06 18:31:13 +0530

    UPdate

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,6 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <ThemeProvider>
-    
     <Stack screenOptions={{headerShown: false}}>
     <Stack.Screen name = "(tabs)"/>
   </Stack>
