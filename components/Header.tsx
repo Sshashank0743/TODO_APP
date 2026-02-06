6e26798b1dd5d7ab80584d4831d82810fac4dc58
shashank shukla <dsshuklashashank@gmail.com>
2026-02-06 22:30:23 +0530

    Update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,17 @@
+import { createHomeStyles } from "@/assets/styles/home.styles";
 import { api } from "@/convex/_generated/api";
+import useTheme from "@/hooks/useTheme";
 import { useQuery } from "convex/react";
 import { Text, View } from "react-native";
 
 const Header = () => {
+    const {colors} = useTheme();
+
+    const homeStyles = createHomeStyles(colors)
 
     const todos = useQuery(api.todos.getTodos);
     return (
-        <View>  
+        <View style = {homeStyles.header}>  
             <Text>Header</Text>
         </View>
     );
